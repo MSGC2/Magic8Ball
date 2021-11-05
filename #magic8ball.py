d62bacb95475b80cbbe5bec9d139de32992c5db6
MSGC2 <93775467+MSGC2@users.noreply.github.com>
2021-11-05 13:08:28 +0000

    Add files via upload

---
+++ #magic8ball.py
@@ -0,0 +1,528 @@
+# MyMagic8Ball
+
+import random
+
+# wirte answers
+ans1="go for it!"
+ans2="no way, Jose!"
+ans3="I'm not sure. Ask me again."
+ans4="fear of the unknown is what imprisons us."
+ans5="it would be madness to do that!"
+ans6="only you can save mankind!"
+ans7="makes no difference to me, do or don't - whatever."
+ans8="yes, i think on balance that si the right choice."
+
+print("welcome to the magic8ball.")
+
+#get the users question
+question = input("ask me for advise then press ENTER to shake.\n")
+
+print("shaking ...\n" * 4)
+#use the randint() function to select the correct answer
+choice=random.randint(1, 8)
+if choice==1:
+    answer=ans1
+elif choice == 2:
+     answer=ans2
+elif choice == 3:
+     answer=ans3
+elif choice == 4:
+     answer=ans4
+elif choice == 5:
+     answer=ans5
+elif choice == 6:
+     answer=ans6
+elif choice == 7:
+     answer=ans7
+else:
+    answer=ans8
+
+# print the answer to the screen
+print(answer)
+
+input("\n\nPress the RETURN key to restart.")
+
+# MyMagic8Ball
+
+import random
+
+# wirte answers
+ans1="go for it!"
+ans2="no way, Jose!"
+ans3="I'm not sure. Ask me again."
+ans4="fear of the unknown is what imprisons us."
+ans5="it would be madness to do that!"
+ans6="only you can save mankind!"
+ans7="makes no difference to me, do or don't - whatever."
+ans8="yes, i think on balance that si the right choice."
+
+print("welcome to the magic8ball.")
+
+#get the users question
+question = input("ask me for advise then press ENTER to shake.\n")
+
+print("shaking ...\n" * 4)
+#use the randint() function to select the correct answer
+choice=random.randint(1, 8)
+if choice==1:
+    answer=ans1
+elif choice == 2:
+     answer=ans2
+elif choice == 3:
+     answer=ans3
+elif choice == 4:
+     answer=ans4
+elif choice == 5:
+     answer=ans5
+elif choice == 6:
+     answer=ans6
+elif choice == 7:
+     answer=ans7
+else:
+    answer=ans8
+
+# print the answer to the screen
+print(answer)
+
+input("\n\nPress the RETURN key to restart.")
+
+# MyMagic8Ball
+
+import random
+
+# wirte answers
+ans1="go for it!"
+ans2="no way, Jose!"
+ans3="I'm not sure. Ask me again."
+ans4="fear of the unknown is what imprisons us."
+ans5="it would be madness to do that!"
+ans6="only you can save mankind!"
+ans7="makes no difference to me, do or don't - whatever."
+ans8="yes, i think on balance that si the right choice."
+
+print("welcome to the magic8ball.")
+
+#get the users question
+question = input("ask me for advise then press ENTER to shake.\n")
+
+print("shaking ...\n" * 4)
+#use the randint() function to select the correct answer
+choice=random.randint(1, 8)
+if choice==1:
+    answer=ans1
+elif choice == 2:
+     answer=ans2
+elif choice == 3:
+     answer=ans3
+elif choice == 4:
+     answer=ans4
+elif choice == 5:
+     answer=ans5
+elif choice == 6:
+     answer=ans6
+elif choice == 7:
+     answer=ans7
+else:
+    answer=ans8
+
+# print the answer to the screen
+print(answer)
+
+input("\n\nPress the RETURN key to restart.")
+
+# MyMagic8Ball
+
+import random
+
+# wirte answers
+ans1="go for it!"
+ans2="no way, Jose!"
+ans3="I'm not sure. Ask me again."
+ans4="fear of the unknown is what imprisons us."
+ans5="it would be madness to do that!"
+ans6="only you can save mankind!"
+ans7="makes no difference to me, do or don't - whatever."
+ans8="yes, i think on balance that si the right choice."
+
+print("welcome to the magic8ball.")
+
+#get the users question
+question = input("ask me for advise then press ENTER to shake.\n")
+
+print("shaking ...\n" * 4)
+#use the randint() function to select the correct answer
+choice=random.randint(1, 8)
+if choice==1:
+    answer=ans1
+elif choice == 2:
+     answer=ans2
+elif choice == 3:
+     answer=ans3
+elif choice == 4:
+     answer=ans4
+elif choice == 5:
+     answer=ans5
+elif choice == 6:
+     answer=ans6
+elif choice == 7:
+     answer=ans7
+else:
+    answer=ans8
+
+# print the answer to the screen
+print(answer)
+
+input("\n\nPress the RETURN key to restart.")
+
+
+# MyMagic8Ball
+
+import random
+
+# wirte answers
+ans1="go for it!"
+ans2="no way, Jose!"
+ans3="I'm not sure. Ask me again."
+ans4="fear of the unknown is what imprisons us."
+ans5="it would be madness to do that!"
+ans6="only you can save mankind!"
+ans7="makes no difference to me, do or don't - whatever."
+ans8="yes, i think on balance that si the right choice."
+
+print("welcome to the magic8ball.")
+
+#get the users question
+question = input("ask me for advise then press ENTER to shake.\n")
+
+print("shaking ...\n" * 4)
+#use the randint() function to select the correct answer
+choice=random.randint(1, 8)
+if choice==1:
+    answer=ans1
+elif choice == 2:
+     answer=ans2
+elif choice == 3:
+     answer=ans3
+elif choice == 4:
+     answer=ans4
+elif choice == 5:
+     answer=ans5
+elif choice == 6:
+     answer=ans6
+elif choice == 7:
+     answer=ans7
+else:
+    answer=ans8
+
+# print the answer to the screen
+print(answer)
+
+input("\n\nPress the RETURN key to restart.")
+
+# MyMagic8Ball
+
+import random
+
+# wirte answers
+ans1="go for it!"
+ans2="no way, Jose!"
+ans3="I'm not sure. Ask me again."
+ans4="fear of the unknown is what imprisons us."
+ans5="it would be madness to do that!"
+ans6="only you can save mankind!"
+ans7="makes no difference to me, do or don't - whatever."
+ans8="yes, i think on balance that si the right choice."
+
+print("welcome to the magic8ball.")
+
+#get the users question
+question = input("ask me for advise then press ENTER to shake.\n")
+
+print("shaking ...\n" * 4)
+#use the randint() function to select the correct answer
+choice=random.randint(1, 8)
+if choice==1:
+    answer=ans1
+elif choice == 2:
+     answer=ans2
+elif choice == 3:
+     answer=ans3
+elif choice == 4:
+     answer=ans4
+elif choice == 5:
+     answer=ans5
+elif choice == 6:
+     answer=ans6
+elif choice == 7:
+     answer=ans7
+else:
+    answer=ans8
+
+# print the answer to the screen
+print(answer)
+
+input("\n\nPress the RETURN key to restart.")
+
+# MyMagic8Ball
+
+import random
+
+# wirte answers
+ans1="go for it!"
+ans2="no way, Jose!"
+ans3="I'm not sure. Ask me again."
+ans4="fear of the unknown is what imprisons us."
+ans5="it would be madness to do that!"
+ans6="only you can save mankind!"
+ans7="makes no difference to me, do or don't - whatever."
+ans8="yes, i think on balance that si the right choice."
+
+print("welcome to the magic8ball.")
+
+#get the users question
+question = input("ask me for advise then press ENTER to shake.\n")
+
+print("shaking ...\n" * 4)
+#use the randint() function to select the correct answer
+choice=random.randint(1, 8)
+if choice==1:
+    answer=ans1
+elif choice == 2:
+     answer=ans2
+elif choice == 3:
+     answer=ans3
+elif choice == 4:
+     answer=ans4
+elif choice == 5:
+     answer=ans5
+elif choice == 6:
+     answer=ans6
+elif choice == 7:
+     answer=ans7
+else:
+    answer=ans8
+
+# print the answer to the screen
+print(answer)
+
+input("\n\nPress the RETURN key to restart.")
+
+# MyMagic8Ball
+
+import random
+
+# wirte answers
+ans1="go for it!"
+ans2="no way, Jose!"
+ans3="I'm not sure. Ask me again."
+ans4="fear of the unknown is what imprisons us."
+ans5="it would be madness to do that!"
+ans6="only you can save mankind!"
+ans7="makes no difference to me, do or don't - whatever."
+ans8="yes, i think on balance that si the right choice."
+
+print("welcome to the magic8ball.")
+
+#get the users question
+question = input("ask me for advise then press ENTER to shake.\n")
+
+print("shaking ...\n" * 4)
+#use the randint() function to select the correct answer
+choice=random.randint(1, 8)
+if choice==1:
+    answer=ans1
+elif choice == 2:
+     answer=ans2
+elif choice == 3:
+     answer=ans3
+elif choice == 4:
+     answer=ans4
+elif choice == 5:
+     answer=ans5
+elif choice == 6:
+     answer=ans6
+elif choice == 7:
+     answer=ans7
+else:
+    answer=ans8
+
+# print the answer to the screen
+print(answer)
+
+input("\n\nPress the RETURN key to restart.")
+
+# MyMagic8Ball
+
+import random
+
+# wirte answers
+ans1="go for it!"
+ans2="no way, Jose!"
+ans3="I'm not sure. Ask me again."
+ans4="fear of the unknown is what imprisons us."
+ans5="it would be madness to do that!"
+ans6="only you can save mankind!"
+ans7="makes no difference to me, do or don't - whatever."
+ans8="yes, i think on balance that si the right choice."
+
+print("welcome to the magic8ball.")
+
+#get the users question
+question = input("ask me for advise then press ENTER to shake.\n")
+
+print("shaking ...\n" * 4)
+#use the randint() function to select the correct answer
+choice=random.randint(1, 8)
+if choice==1:
+    answer=ans1
+elif choice == 2:
+     answer=ans2
+elif choice == 3:
+     answer=ans3
+elif choice == 4:
+     answer=ans4
+elif choice == 5:
+     answer=ans5
+elif choice == 6:
+     answer=ans6
+elif choice == 7:
+     answer=ans7
+else:
+    answer=ans8
+
+# print the answer to the screen
+print(answer)
+
+input("\n\nPress the RETURN key to restart.")
+
+# MyMagic8Ball
+
+import random
+
+# wirte answers
+ans1="go for it!"
+ans2="no way, Jose!"
+ans3="I'm not sure. Ask me again."
+ans4="fear of the unknown is what imprisons us."
+ans5="it would be madness to do that!"
+ans6="only you can save mankind!"
+ans7="makes no difference to me, do or don't - whatever."
+ans8="yes, i think on balance that si the right choice."
+
+print("welcome to the magic8ball.")
+
+#get the users question
+question = input("ask me for advise then press ENTER to shake.\n")
+
+print("shaking ...\n" * 4)
+#use the randint() function to select the correct answer
+choice=random.randint(1, 8)
+if choice==1:
+    answer=ans1
+elif choice == 2:
+     answer=ans2
+elif choice == 3:
+     answer=ans3
+elif choice == 4:
+     answer=ans4
+elif choice == 5:
+     answer=ans5
+elif choice == 6:
+     answer=ans6
+elif choice == 7:
+     answer=ans7
+else:
+    answer=ans8
+
+# print the answer to the screen
+print(answer)
+
+input("\n\nPress the RETURN key to restart.")
+
+# MyMagic8Ball
+
+import random
+
+# wirte answers
+ans1="go for it!"
+ans2="no way, Jose!"
+ans3="I'm not sure. Ask me again."
+ans4="fear of the unknown is what imprisons us."
+ans5="it would be madness to do that!"
+ans6="only you can save mankind!"
+ans7="makes no difference to me, do or don't - whatever."
+ans8="yes, i think on balance that si the right choice."
+
+print("welcome to the magic8ball.")
+
+#get the users question
+question = input("ask me for advise then press ENTER to shake.\n")
+
+print("shaking ...\n" * 4)
+#use the randint() function to select the correct answer
+choice=random.randint(1, 8)
+if choice==1:
+    answer=ans1
+elif choice == 2:
+     answer=ans2
+elif choice == 3:
+     answer=ans3
+elif choice == 4:
+     answer=ans4
+elif choice == 5:
+     answer=ans5
+elif choice == 6:
+     answer=ans6
+elif choice == 7:
+     answer=ans7
+else:
+    answer=ans8
+
+# print the answer to the screen
+print(answer)
+
+input("\n\nPress the RETURN key to restart.")
+
+# MyMagic8Ball
+
+import random
+
+# wirte answers
+ans1="go for it!"
+ans2="no way, Jose!"
+ans3="I'm not sure. Ask me again."
+ans4="fear of the unknown is what imprisons us."
+ans5="it would be madness to do that!"
+ans6="only you can save mankind!"
+ans7="makes no difference to me, do or don't - whatever."
+ans8="yes, i think on balance that si the right choice."
+
+print("welcome to the magic8ball.")
+
+#get the users question
+question = input("ask me for advise then press ENTER to shake.\n")
+
+print("shaking ...\n" * 4)
+#use the randint() function to select the correct answer
+choice=random.randint(1, 8)
+if choice==1:
+    answer=ans1
+elif choice == 2:
+     answer=ans2
+elif choice == 3:
+     answer=ans3
+elif choice == 4:
+     answer=ans4
+elif choice == 5:
+     answer=ans5
+elif choice == 6:
+     answer=ans6
+elif choice == 7:
+     answer=ans7
+else:
+    answer=ans8
+
+# print the answer to the screen
+print(answer)
+
+input("\n\nPress the RETURN key to restart.")
